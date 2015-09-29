890f9ffba3a751c73940ea9d84f8b1f36ad39086
Isaac <isaac.r.enderline@gmail.com>
2015-09-29 16:21:05 -0400

    added readme

--- setup/initial.sh
+++ setup/initial.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 apt-get -y update
+# Install basic tools
 apt-get install -qq curl unzip git-core ack-grep software-properties-common build-essential
+# Update
 apt-get -y update
+# Install apache, php, samba
 apt-get -y install apache2 php5 samba
+# restart apache
 service apache2 restart
+# add mysql repo
 add-apt-repository -y ppa:ondrej/mysql-5.6
+# update again
 apt-get -y update
+# Mysql set some defaults
 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+# install mysql
 apt-get install -qq mysql-server-5.6
+# mysql available to everyone, so you can reach it from you PC running vagrant
 sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+# set up some mysql users
 /usr/bin/mysql -uroot -proot -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;FLUSH PRIVILEGES;"
 /usr/bin/mysql -uroot -proot -e "CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'vagrant'"
 /usr/bin/mysql -uroot -proot -e "GRANT ALL ON *.* TO 'vagrant'@'localhost' IDENTIFIED BY 'vagrant'"
+# Mysql install complete so lets retart, YAY!
 service mysql restart
+# What the hell, let update again
 apt-get -y update
+# line below is useles, does nothing
+whoami
